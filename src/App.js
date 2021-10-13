289c2710bc93d479049ac66270ded33410a16ead
Ishan <1905614@kiit.ac.in>
2021-10-13 19:24:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./components/Header";
+import { useState, useEffect } from "react";
+import Sidebar from "./components/Sidebar";
+import MainContent from "./components/MainContent";
 function App() {
+  const [animeList, setAnimeList] = useState([]);
+  const [topAnime, setTopAnime] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const GetTopAnime = async () => {
+    const temp = await fetch(
+      `https://api.jikan.moe/v3/top/anime/1/bypopularity`
+    ).then((res) => res.json());
+
+    setTopAnime(temp.top.slice(0, 5));
+  };
+
+  const HandleSearch = (e) => {
+    e.preventDefault();
+    // console.log(search);
+    FetchAnime(search);
+  };
+  const FetchAnime = async (query) => {
+    const temp = await fetch(
+      `https://api.jikan.moe/v3/search/anime?q=${query}&order_by=title&sort=asc&limit=10`
+    ).then((res) => res.json());
+    setAnimeList(temp.results);
+  };
+
+  useEffect(() => {
+    GetTopAnime();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="content-wrap">
+        <Sidebar topAnime={topAnime} />
+        <MainContent
+          HandleSearch={HandleSearch}
+          search={search}
+          setSearch={setSearch}
+          animeList={animeList}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import 'animate.css';
+function Header() {
+  return (
+    <header>
+      <h1 class="animate__animated animate__bounce">
+        The
+        <strong>Anime</strong>
+        Database
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
